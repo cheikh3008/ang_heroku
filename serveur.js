0df9f280198e526d76675a04b61b85ef8c7b760d
cheikh3008 <cheikh3008@gmail.com>
2021-07-28 19:03:24 +0000

    first commit

--- serveur.js
+++ serveur.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 app.use(express.static(__dirname + 'dist/resthaut-front'));
 app.get('/*', function (req, res) {
   res.sendFile(path.json(__dirname + 'dist/resthaut-front/index.html'))
 });
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
